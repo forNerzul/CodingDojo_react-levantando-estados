6ee2e76c73a5af7bd0e96d4470e608a64a7c5e0c
forNerzul <sbeardman92@gmail.com>
2023-01-19 20:59:25 -0300

    checkpoint commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+// Digamos que queremos hacer una aplicación que contenga un componente para crear mensajes y otro componente para mostrar mensajes.
+
+const MessageForm = (props) => {
+    const [msg, setMsg] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // qué debemos hacer con el mensaje?
+        props.onNewMessage(msg);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Set Message</h1>
+            <textarea
+                rows="4"
+                cols="50"
+                placeholder="Enter your message here"
+                onChange={(e) => setMsg(e.target.value)}
+                value={msg}
+            ></textarea>
+            <input type="submit" value="Send Message" />
+        </form>
+    );
+};
+
+const MessageDisplay = (props) => {
+    return (
+        <>
+            <h1>Current Message</h1>
+            <pre>{props.message}</pre>
+        </>
+    );
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentMsg, setCurrentMsg] = useState("There are no messages");
+
+    const youveGotMail = (newMessage) => {
+        setCurrentMsg(newMessage);
+    };
+
+    return (
+        <>
+            <MessageForm onNewMessage={youveGotMail} />
+            <MessageDisplay message={currentMsg} />
+        </>
+    );
 }
 
 export default App;
